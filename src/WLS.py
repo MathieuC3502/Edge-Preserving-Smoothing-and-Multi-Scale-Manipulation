ac7bd3ef0bff554c65bb949c5d97fd1e0747e1c6
MathieuC3502 <mathieuchesneau35@gmail.com>
2023-05-22 23:55:45 +0200

    Minor bug corrections
    Fixed display issue (range of values)
    Reworked some formulas for better readability
    Implemented final display with linked axis between original and smoothed image

--- src/WLS.py
+++ src/WLS.py
@@ -30,21 +30,11 @@ def extraction_image(nomFichier):
 
 # Definition of the forward difference operators
 
-def deriveur_x(n):
-    """
-    Renvoie une matrice de taille n*n qui permet de dériver un vecteur de taille n
-    """
-    matrice = np.zeros((n, n))
-    for i in range(n):
-        matrice[i][i] = 2
-        if i != n - 1:
-            matrice[i][i + 1] = -1
-        if i != 0:
-            matrice[i][i - 1] = -1
-    return matrice
+def deriveur_x(img_rgb,plane,i,j):
+    return abs((2*img[i,j,plane])-img[i-1,j,plane]-img[i+1,j,plane])
 
-def deriveur_y(n):
-    return (deriveur_x(n).transpose)
+def deriveur_y(img_rgb,plane,i,j):
+    return abs((2*img[i,j,plane])-img[i,j-1,plane]-img[i,j+1,plane])
 
 #---------------------------------------------------------------
 
@@ -75,23 +65,20 @@ for i in range(row):
 #---------------------------------------------------------------
 
 ax=np.zeros([row,col,3])
-ay=np.zeros([row,col,3])     
-alpha=1.2
-epsilon=0.0001
+ay=np.zeros([col,row,3])
 
 for i in range(1,row-1):
     for j in range(1,col-1):
-        ax[i,j,0]=((abs(2*(img[i,j,0])-img[i-1,j,0]-img[i+1,j,0])**alpha)+epsilon)**-1
-        ay[i,j,0]=((abs(2*(img[i,j,0])-img[i,j-1,0]-img[i,j-1,0])**alpha)+epsilon)**-1
+        ax[i,j,0]=((deriveur_x(img,0,i,j)**alpha)+epsilon)**-1
+        ay[j,i,0]=((deriveur_y(img,0,i,j)**alpha)+epsilon)**-1
         
-        ax[i,j,1]=((abs(2*(img[i,j,1])-img[i-1,j,1]-img[i+1,j,1])**alpha)+epsilon)**-1
-        ay[i,j,1]=((abs(2*(img[i,j,1])-img[i,j-1,1]-img[i,j-1,1])**alpha)+epsilon)**-1
+        ax[i,j,1]=((deriveur_x(img,1,i,j)**alpha)+epsilon)**-1
+        ay[j,i,1]=((deriveur_y(img,1,i,j)**alpha)+epsilon)**-1
         
-        ax[i,j,2]=((abs(2*(img[i,j,2])-img[i-1,j,2]-img[i+1,j,2])**alpha)+epsilon)**-1
-        ay[i,j,2]=((abs(2*(img[i,j,2])-img[i,j-1,2]-img[i,j-1,2])**alpha)+epsilon)**-1
+        ax[i,j,2]=((deriveur_x(img,2,i,j)**alpha)+epsilon)**-1
+        ay[j,i,2]=((deriveur_y(img,0,i,j)**alpha)+epsilon)**-1
         
 ax_vec=ax.reshape(1,nbr_pix,3)
-ay=np.transpose(ay)
 ay_vec=ay.reshape(1,nbr_pix,3)
 img_vec=img.reshape(1,nbr_pix,3)
 
@@ -154,7 +141,6 @@ AYG=ssp.diags(ay_vec[:,:,1],[0])
 AXB=ssp.diags(ax_vec[:,:,2],[0])
 AYB=ssp.diags(ay_vec[:,:,2],[0])
 
-lbda=2
 Id=ssp.identity(AXB.shape[1])
 
 #---------------------------------------------------------------
@@ -178,11 +164,18 @@ New_Img_G=ssp.linalg.spsolve(HG,np.transpose(img_vec[:,:,1]))
 New_Img_B=ssp.linalg.spsolve(HB,np.transpose(img_vec[:,:,2]))
 
 New_Img=np.zeros([1,nbr_pix,3])
-New_Img[:,:,0]=New_Img_R
-New_Img[:,:,1]=New_Img_G
-New_Img[:,:,2]=New_Img_B
+New_Img[:,:,0]=(1/255)*New_Img_R
+New_Img[:,:,1]=(1/255)*New_Img_G
+New_Img[:,:,2]=(1/255)*New_Img_B
 
 New_Img=New_Img.reshape([row,col,3])
 
-plt.imshow(New_Img)
+ax1=plt.subplot(122)
+ax1.imshow(New_Img)
+plt.title("Smoothed Image")
+plt.show()
+
+plt.subplot(121,sharex=ax1, sharey=ax1)
+plt.imshow(img)
+plt.title("Original Image")
 plt.show()
