27fbd3a7c79abf58e27c7eca33a5cd498766b163
Mathieu Chesneau <mathieuchesneau35@gmail.com>
2023-06-04 21:13:16 +0200

    Update and rename Clean_WLS.py to WLS.py

--- src/WLS.py
+++ src/WLS.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import time
-import Clean_Functions as Functions
+import Functions as Functions
 
 #---------------------------------------------------------------
 # LOADING OF THE DIFFERENT TEST IMAGES
